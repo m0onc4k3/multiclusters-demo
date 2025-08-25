0828c9103ecc105368b242d389514abe4613018a
taggioml <jdprzd@gmail.com>
2025-08-25 09:24:20 +0000

    Updated celery-worker

--- 03-microservices/subscription_registration/subscription_registration/settings_worker.py
+++ 03-microservices/subscription_registration/subscription_registration/settings_worker.py
@@ -10,5 +10,7 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {'console': {'class': 'logging.StreamHandler'}},
-    'loggers': {'': {'handlers': ['console'], 'level': 'INFO'}},
+    'loggers': {'': {'handlers': ['console'], 'level': 'DEBUG'}},
 }
+
+REDIS_HOST= 'my-redis'
